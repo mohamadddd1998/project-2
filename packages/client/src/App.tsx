a9136d0d2706b17f7f0b1f76082b94e68dd8269d
mohamad <mohamad@gmail.com>
2025-11-01 10:13:23 +0330

    add submodule

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Button from '../../core/packages/ui/src/button'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <Button></Button>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
