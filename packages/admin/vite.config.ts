e7c9693c76da8b4900b0f4760734620b0c35e373
mohamad <mohamad@gmail.com>
2025-11-01 09:51:43 +0330

    stable

--- packages/admin/vite.config.ts
+++ packages/admin/vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "#base": path.resolve(__dirname, "../base/src"),
+      "#components": path.resolve(__dirname, "../ui/src/components"),
+      "#ui": path.resolve(__dirname, "../ui/src"),
+      "#shared": path.resolve(__dirname, "../shared/src"),
+      "#admin": path.resolve(__dirname, "./src"),
+    },
+  },
+});
